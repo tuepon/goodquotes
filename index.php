929a6b792d93cef5da3c9ed6f17e7d0f48a527a5
Tomonori Ueno <t.uepon@gmail.com>
2019-03-03 21:55:18 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>GoodQuotes</title>
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <link rel="stylesheet" href="./css/style.css">
+  </head>
+  <body>
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills float-right">
+            <li class="nav-item"><a href="index.php" class="nav-link active">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">New Quote</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted">GoodQuotes</h3>
+      </div>
+
+      <div class="jumbotron">
+        <h1>Got A Quote?</h1>
+        <p class="lead">Store your favorite quotes here to access and read them daily and better your life</p>
+        <p><a class="btn btn-lg btn-success" href="new.php" role="button">Add Quote Now</a></p>
+      </div>
+
+      <div class="row marketing">
+        <div class="col-lg-12">
+          <h3>Subheading</h3>
+          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+
+          <h3>Subheading</h3>
+          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+
+          <h3>Subheading</h3>
+          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+        </div>
+      </div>
+
+      <footer class="footer">
+        <p>&copy; 2019 GoodQuotes, Inc.</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+  </body>
+</html>
